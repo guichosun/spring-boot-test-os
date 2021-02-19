c4f1892f359956cef1c0a91a0ee797117b466b90
alberto.delcampo-at-676770050448 <luis.albertodelcampo@gmail.com>
2021-02-19 15:18:34 -0600

    Todo el código del ejemplo

---
+++ src/main/java/com/pluralsight/health/MaxMemoryHealtIndicator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.pluralsight.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author guichosun
+ *
+ */
+@Component
+public class MaxMemoryHealtIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		boolean invalid = Runtime.getRuntime().maxMemory() < (100 * 1024 * 1024);
+		Status status = invalid ? Status.DOWN : Status.UP;
+		return Health.status(status).build();
+	}
+
+}
